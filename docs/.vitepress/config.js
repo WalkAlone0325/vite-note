afd7ac4cb6b98af98507f9ade62e0efff739f824
Nebula <jsw0325@163.com>
2022-08-08 10:02:29 +0800

    feat: add vi.md

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -54,6 +54,7 @@ export default defineConfig({
           items: [
             { text: "Mac 开发环境配置", link: "/util/mac-utils" },
             { text: "Linux 常用命令", link: "/util/linux" },
+            { text: "Vi 常用命令", link: "/util/vi" },
           ],
         },
       ],
